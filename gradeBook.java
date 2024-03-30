7fb611990b748599d6ebf2d850f5ae5aa5da72e5
Halimah <127022084+halimahexe@users.noreply.github.com>
2024-03-30 23:20:49 +0000

    Create gradeBook.java

---
+++ gradeBook.java
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/55cbd4ba903825f7970000f5/train/java
+
+public class GrassHopper {
+    public static char getGrade(int s1, int s2, int s3) {
+      int average = (s1 + s2 + s3) / 3;
+      if (average >= 90) {
+        return 'A';
+      } else if (average >= 80) {
+        return 'B';
+      } else if (average >= 70) {
+        return 'C';
+      } else if (average >= 60) {
+        return 'D';
+      } else return 'F';
+    }
+}
