68b14ce90c0e72e4e7092362ba66f6cae99290ad
Halimah <127022084+halimahexe@users.noreply.github.com>
2024-03-30 21:59:01 +0000

    Create checkForFactor.java

---
+++ checkForFactor.java
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/java
+
+public class Kata {
+    public static boolean checkForFactor(int base, int factor) {
+        return (base % factor == 0);
+    }
+}
