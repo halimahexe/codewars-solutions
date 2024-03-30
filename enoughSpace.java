1a106b7ca41dab19a608bafc4db788159262dfb4
Halimah <127022084+halimahexe@users.noreply.github.com>
2024-03-30 23:31:16 +0000

    Create enoughSpace.java

---
+++ enoughSpace.java
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5875b200d520904a04000003/train/java
+
+public class Bob {
+  public static int enough(int cap, int on, int wait){
+    int result = (wait - (cap - on));
+    return result > 0 ? result : 0;
+  }
+}
