0907baad0842952ec2e18f4120e63c325a4cad9a
Halimah <127022084+halimahexe@users.noreply.github.com>
2024-04-01 22:00:05 +0100

    Update Kata.java

--- Kata.java
+++ Kata.java
@@ -1,7 +1,7 @@
 // Check for Factor Kata
 // https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/java
 
-public class Kata {
+public class Factor {
     public static boolean checkForFactor(int base, int factor) {
         return (base % factor == 0);
     }
