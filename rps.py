55ac4031888cb42e3b272c0e5399b8b16729070e
Halimah <127022084+halimahexe@users.noreply.github.com>
2024-04-05 20:23:13 +0100

    Refactoring to simplify

--- rps.py
+++ rps.py
@@ -1,9 +1,10 @@
 # https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/python
 
 def rps(p1, p2):
-    if p1 == "scissors" and p2 == "paper" or p1 == "paper" and p2 == "rock" or p1 == "rock" and p2 == "scissors":
+    beats = {"scissors": "paper", "paper": "rock", "rock": "scissors"}
+    if beats[p1] == p2:
         return "Player 1 won!"
-    elif p2 == "scissors" and p1 == "paper" or p2 == "paper" and p1 == "rock" or p2 == "rock" and p1 == "scissors":
+    elif beats[p2] == p1:
         return "Player 2 won!"
     elif p1 == p2:
         return "Draw!"
