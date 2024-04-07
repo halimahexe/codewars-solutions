481e74c4cf1fb4573d3523ee597ca2adc93f2ff9
Halimah <127022084+halimahexe@users.noreply.github.com>
2024-04-07 20:23:37 +0100

    Refactoring to remove unused variable

--- twoSum.js
+++ twoSum.js
@@ -1,8 +1,7 @@
 // https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript
 
 function twoSum(numbers, target) {
-  let list = [];
-  for (let i = 0; i < numbers.length; i++) {
+  for (let i = 0; i < numbers.length-1; i++) {
     for (let j = 1; j < numbers.length; j++) {
       if (numbers[i] + numbers[j] == target) {
         return [i, j];
