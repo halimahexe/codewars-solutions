b793c2881cd3b13fa26f026e7503e0f83ad764fc
Halimah <127022084+halimahexe@users.noreply.github.com>
2024-04-07 18:54:18 +0100

    Create petals.py

---
+++ petals.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/57f24e6a18e9fad8eb000296/train/python
+
+def how_much_i_love_you(nb_petals):
+    phrases = {
+        1: "I love you",
+        2: "a little",
+        3: "a lot",
+        4: "passionately",
+        5: "madly",
+        6: "not at all"
+    }
+    n = nb_petals % 6
+    if n == 0:
+        return phrases[6]
+    else:
+        return phrases[n]
